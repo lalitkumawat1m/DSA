31bc21373b1310cfb95b23dc9b8482d50e7b88cd
Lalit Kumawat <lalitkumawat1129@gmail.com>
2022-11-02 13:11:01 +0530

    Time: 0 ms (100.00%), Space: 40 MB (93.40%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -0,0 +1,100 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+    
+        int level=0;
+        Queue<String>q=new LinkedList<>();
+        int len=bank.length;
+        char []chr={'A','C','G','T'};
+        
+        Set<String>hset=new HashSet<>();
+        for(String s:bank){
+            hset.add(s);
+        }
+        
+        q.add(start);
+        
+        while(true){
+            level++;
+            
+            int n=q.size();
+            if(n==0){
+                return -1;
+            }
+            
+            for(int i=0;i<n;i++){
+                char ch[]=q.poll().toCharArray();
+                
+                for(int j=0;j<8;j++){
+                    
+                    char org_char=ch[j];
+                    for(int c=0;c<4;c++){
+                        ch[j]=chr[c];
+                        String str=String.valueOf(ch);
+                        if(str.equals(end)&& hset.contains(str)){
+                            
+                            return level;
+                        }
+                        if(!hset.contains(str)){
+                            continue;
+                        }
+                        hset.remove(str);
+                        q.add(str);
+                    }
+                    ch[j]=org_char;
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+}
+
+
+//  public int minMutation(String start, String end, String[] bank) {
+//         int level = 0;
+//         int len = bank.length;
+//         char[] chr = {'A','C','G','T'};
+            
+//         Set<String> hset = new HashSet<String>();
+//         for(String s: bank){
+//             hset.add(s);
+//         }
+        
+//         Queue<String> queue = new LinkedList<>();
+//         queue.add(start);
+        
+//         while(true){
+//             level++;
+//             int n = queue.size();
+            
+//             if(n == 0){
+//                 return -1;
+//             }
+            
+//             for(int i =0; i < n; i++){
+//                 char[] ch = queue.poll().toCharArray();
+                
+//                 for(int j =0; j < 8; j++){
+//                     char org_char = ch[j];
+//                     for(int c = 0; c<4; c++){
+//                         ch[j] = chr[c]; 
+//                         String str = String.valueOf(ch);
+//                         if(str.equals(end)
+//                           && hset.contains(str)){
+//                             return level;
+//                         }
+                        
+//                         if(!hset.contains(str)){
+//                             continue;
+//                         }
+                        
+//                         hset.remove(str);
+//                         queue.add(str);
+//                     }
+//                     ch[j] = org_char;
+//                 }
+//             }
+//         }
+//     }
